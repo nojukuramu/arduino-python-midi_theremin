9be8d68032f8b11def8ad87866f62727b073ff5c
nojukuramu <119827430+nojukuramu@users.noreply.github.com>
2024-01-12 15:42:04 +0800

    Just added some comments to theremin-v1.0.py

--- theremin-v1.0.py
+++ theremin-v1.0.py
@@ -1,7 +1,8 @@
-import serial
-import rtmidi
-import statistics
+import serial #serial library
+import rtmidi #rtmidi library
+import statistics #statistics useful for getting median later
 
+#initialization of the midi
 midiout = rtmidi.MidiOut()
 available_ports = midiout.get_ports()
 
@@ -16,6 +17,7 @@ else:
 
 ser = serial.Serial('COM4', 9600)
 
+#list of notes is for the notes i want to send as midi notes. idstance are just gates to avoid shaky values
 list_of_notes = [65, 66, 68, 70, 72, 73, 75, 77, 78, 80, 82]
 distance_gate = [15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55]
 
@@ -23,36 +25,44 @@ distance_gate = [15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55]
  # Set MIDI channel to 1 explicitly in the status byte
 status_byte = 0x90 | 0x00  # 0x90 is the note-on status, 0x00 is the MIDI channel (1)
 
+#initialization of midichannel and volume_cc number
 midi_channel = 1
 volume_cc = 7
 
+#function for sending the volume value as midi
 def set_volume(value):
     midiout.send_message([0xB0 | (midi_channel - 1), volume_cc, value])
 
+#basically just the map() of arduino library
 def map(x, in_min, in_max, out_min, out_max):
   return round((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
+#smoother of the inputs
 def smoother(input_list):
     return round((sum(input_list)/len(input_list)))
 
+#try block to catch errors
 try:
+    #initialization. dont change value unless you know what you are doing
     active_notes = set()
     snap_note = 0
     index = 0
     note_list = set()
     vol_list = set()
     cycler = 0
+    #loop to read from serial monitor and send midi inputs
     while True:
-        line = ser.readline().decode('utf-8')
-        list_line = line.split()
-        note_line = int(list_line[0])
-        vol_line = int(list_line[1])
-        mapvol = map(vol_line, 9, 30, 0, 127)
+        line = ser.readline().decode('utf-8')   #read serial monitor
+        list_line = line.split()                #splitting and putting it on a list
+        note_line = int(list_line[0])           #you know what this does
+        vol_line = int(list_line[1])            #this onoe too
+        mapvol = map(vol_line, 9, 30, 0, 127)   #remapping sonar output to midi inputs
 
         #keep mapvol above 0
         if mapvol < 0:
             mapvol = 0
-        
+
+        #collects input data and select the lower median
         if cycler < 10:
             note_list.add(note_line)
             vol_list.add(mapvol)
